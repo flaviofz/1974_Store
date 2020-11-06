9a78711a31e73d0e17cd20b9de7dcfb3d113407b
flaviofz <flaviofz@gmail.com>
2020-11-06 06:54:34 -0300

    30 - Aplicando Validações

--- Store.Domain/StoreContext/Commands/OrderCommands/Input/CreateOrderCommand.cs
+++ Store.Domain/StoreContext/Commands/OrderCommands/Input/CreateOrderCommand.cs
@@ -1,12 +1,31 @@
 using System;
 using System.Collections.Generic;
+using FluentValidator;
+using FluentValidator.Validation;
+using Store.Shared.Commands;
 
 namespace Store.Domain.StoreContext.Commands.OrderCommands.Input
 {
-    public class CreateOrderCommand
+    public class CreateOrderCommand : Notifiable, ICommand
     {
+        public CreateOrderCommand()
+        {
+            OrderItems = new List<OrderItemCommand>();
+        }
+
         public Guid IdCustomer { get; set; }
         public IList<OrderItemCommand> OrderItems { get; set; }
+
+        public bool Valid()
+        {
+            AddNotifications(new ValidationContract()
+                .Requires()
+                .HasLen(IdCustomer.ToString(), 36, "Customer", "Identificador do cliente inválido")
+                .IsGreaterThan(OrderItems.Count, 0, "Items", "Nenhum ítem do pedido foi encontrado")
+            );
+
+            return Valid();
+        }
     }
 
     public class OrderItemCommand
